88d1256eaf82158a9055815e6f56f65642763a83
jininc3 <jininc3@gmail.com>
2024-07-08 00:19:06 +0100

    hellolol

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -6,7 +6,7 @@ function Section1({ handleScroll, isButtonVisible }) {
   return (
     <div id="section1" className="section">
       <Link to="/"><h1 className="title-home">Motivdle</h1></Link>
-      <Link to="/"><p className="subtitle-home">Inspire. Empower. Achieve.</p></Link>
+      <Link to="/"><p className="subtitle-home">Daily Motivation to inspire.</p></Link>
       {isButtonVisible && (
         <button id="scrollButton" className="test-button" onClick={handleScroll}>Motivational Quote</button>
       )}
