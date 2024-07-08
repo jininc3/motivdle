4ad91eccdc2cad49148e66b1e8e04420fa00ead1
jininc3 <jininc3@gmail.com>
2024-07-08 22:44:17 +0100

    more done

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -5,6 +5,7 @@ import './Section1.css';
 function Section1({ handleScroll, isButtonVisible }) {
   return (
     <div id="section1" className="section">
+      <img src="/motivdle-logo.png" alt="Logo" className="logo" /> {/* Update the path here */}
       <Link to="/"><h1 className="title-home">Motivdle</h1></Link>
       <Link to="/"><p className="subtitle-home">Daily Motivation to inspire.</p></Link>
       {isButtonVisible && (
