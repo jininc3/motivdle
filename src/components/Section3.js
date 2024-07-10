f0866dec79fe6c1110e36f122b35a41a51de7666
jininc3 <jininc3@gmail.com>
2024-07-10 16:36:04 +0100

    daily version worked!

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import './Section3.css';
 import image1 from '../assets/image1.jpg';
-import image2 from '../assets/image1.jpg';
-import image3 from '../assets/image1.jpg';
-import image4 from '../assets/image1.jpg';
-import image5 from '../assets/image1.jpg';
-import image6 from '../assets/image1.jpg';
+import image2 from '../assets/image2.jpg';
+import image3 from '../assets/image3.jpg';
+import image4 from '../assets/image4.jpg';
+import image5 from '../assets/image5.jpg';
+import image6 from '../assets/image6.jpg';
 
 const images = [image1, image2, image3, image4, image5, image6];
 
--- src/components/Section4.js
+++ src/components/Section4.js
@@ -1,35 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import './Section4.css';
+import React from 'react';
+import './Section4.css'; 
 import { Link } from 'react-router-dom';
 import video1 from '../assets/video1.mp4';
 import video2 from '../assets/video2.mp4';
 import video3 from '../assets/video3.mp4';
-import video4 from '../assets/video4.mp4';
+// Import other videos as needed
 
-const videos = [video1, video2, video3, video4];
+const videoFiles = [
+    video1,
+    video2,
+    video3,
+    // Add more video imports as needed
+];
+
+const getTodaysVideo = () => {
+    const today = new Date();
+    const dayOfYear = Math.floor((today - new Date(today.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+    return videoFiles[dayOfYear % videoFiles.length]; // Cycle through videos
+};
 
 const Section4 = React.forwardRef((props, ref) => {
-  const [video, setVideo] = useState("");
+    const videoSrc = getTodaysVideo();
+    console.log(`Loading video: ${videoSrc}`); // Debugging line
 
-  useEffect(() => {
-    const today = new Date();
-    const videoIndex = today.getDate() % videos.length;
-    setVideo(videos[videoIndex]);
-  }, []);
-
-  return (
-    <div id="section4" className="section" ref={ref}>
-      <div className="overlay-v">
-        <video className="middle-video" controls>
-          <source src={video} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <Link to="/comment">
-          <button className="video-button">Go chase your dreams</button>
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div id="section4" className="section" ref={ref}>
+            <div className="overlay-v">
+                <video className="middle-video" controls>
+                    <source src={videoSrc} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <Link to="/comment">
+                    <button className="video-button">Go chase your dreams</button>
+                </Link>
+            </div>
+        </div>
+    );
 });
 
 export default Section4;
