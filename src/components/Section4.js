cd3608a4d60397c276b89c5c74b7ebcf7fb308b1
jininc3 <jininc3@gmail.com>
2024-07-10 16:42:15 +0100

    hello212

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Section4.css'; 
 import { Link } from 'react-router-dom';
-import video1 from '../assets/video1.mp4';
+import video1 from '../assets/video2.mp4';
 import video2 from '../assets/video2.mp4';
 import video3 from '../assets/video3.mp4';
 // Import other videos as needed
