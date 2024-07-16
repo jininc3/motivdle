c9383a0b304bb67cf24945de68c76c46452a17aa
jininc3 <jininc3@gmail.com>
2024-07-16 23:04:59 +0100

    surely

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './Section3.css';
-import image1 from '../assets/image1.jpg';
+
 import image2 from '../assets/image2.jpg';
 import image3 from '../assets/image3.jpg';
 import image4 from '../assets/image4.jpg';
