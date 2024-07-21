204f8b0bd6cb82398971af4b600cdb6f4ccf8a15
jininc3 <jininc3@gmail.com>
2024-07-21 12:00:08 +0100

    backend6

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";  // Import Firestore
 
 // Your web app's Firebase configuration
@@ -16,7 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);  // Initialize Firestore
 
 // Export Firestore functions
